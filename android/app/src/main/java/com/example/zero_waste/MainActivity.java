c311a6f48247a76888f1431892b62359350a7558
Olaoluwa <tokiolaoluwa01@gmail.com>
2022-05-28 20:12:22 +0100

    Initial Commit

---
+++ android/app/src/main/java/com/example/zero_waste/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.zero_waste;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:zero_waste/screens/splash_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      title: 'Zero Waste',
+      home: SplashScreen(),
+    ),
+  );
+}
