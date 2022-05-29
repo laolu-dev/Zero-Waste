2751535779c1dd4ab4e5b663e1ea92a61024f110
usseiin <sirkenny.hk@gmail.com>
2022-05-29 22:27:13 +0100

    modify the login screen

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 
-String appImage = "images/app_image.svg";
+String appImage = "images/appImage.svg";
+
+String fbSvg = "images/facebook.svg";
+
+String googleSvg = "images/google.svg";
+
+String linklnSvg = "images/linkln.svg";
 
 const inputWhite = Color.fromRGBO(252, 252, 252, 1);
 
 const hintColor = Color.fromRGBO(151, 151, 151, 1);
+
+const inputBorderColor = Color.fromRGBO(226, 226, 226, 1);
 
 TextStyle textStyleOne = const TextStyle(
     color: Color.fromARGB(255, 0, 0, 0),
@@ -13,9 +21,14 @@ TextStyle textStyleOne = const TextStyle(
 
 Container inputContainer({required Widget child, Color? color}) {
   return Container(
-    color: color,
-    height: 52,
-    padding: const EdgeInsets.only(top: 16, bottom: 16, left: 22),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(12),
+      color: color,
+    ),
+    constraints: const BoxConstraints(
+      maxWidth: 400,
+      minHeight: 52,
+    ),
     child: child,
   );
 }
@@ -24,14 +37,18 @@ Color colorGreen = const Color.fromRGBO(10, 157, 86, 1);
 
 Color lineColor = const Color.fromRGBO(22, 22, 29, 1);
 
-InputDecoration inputDecoration(
-    {required String hintText, required String labelText}) {
+InputDecoration inputDecoration({required String labelText}) {
   return InputDecoration(
     labelStyle: const TextStyle(
         fontSize: 14, fontWeight: FontWeight.w400, color: hintColor),
-    hintText: hintText,
     labelText: labelText,
-    border: InputBorder.none,
+    contentPadding: const EdgeInsets.only(top: 16, bottom: 16, left: 22),
+    focusedBorder: OutlineInputBorder(
+      borderSide: BorderSide(color: colorGreen),
+    ),
+    border: const OutlineInputBorder(
+      borderSide: BorderSide(color: inputBorderColor),
+    ),
   );
 }
 
--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 import 'package:zero_waste/constants/constant.dart';
 
@@ -14,7 +15,7 @@ class LoginScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Image.asset(appImage, width: 75, height: 75),
+              SvgPicture.asset(appImage, width: 75, height: 75),
               const SizedBox(height: 15),
               Text(
                 "Login to your Account",
@@ -25,7 +26,6 @@ class LoginScreen extends StatelessWidget {
                 color: inputWhite,
                 child: TextField(
                   decoration: inputDecoration(
-                    hintText: "Email",
                     labelText: "Email",
                   ),
                 ),
@@ -34,50 +34,65 @@ class LoginScreen extends StatelessWidget {
               inputContainer(
                 color: inputWhite,
                 child: TextField(
-                  decoration: inputDecoration(
-                      hintText: "Password", labelText: "Password"),
+                  decoration: inputDecoration(labelText: "Password"),
                 ),
               ),
               const SizedBox(height: 16),
-              const Text("Forget password"),
+              Container(
+                constraints:
+                    const BoxConstraints.expand(height: 15, width: 400),
+                child: Text(
+                  "Forget password",
+                  style: TextStyle(color: colorGreen, fontSize: 14),
+                ),
+              ),
               const SizedBox(height: 24),
-              inputContainer(
+              Container(
+                height: 52,
+                constraints: const BoxConstraints(maxWidth: 400),
                 child: ElevatedButton(
                   onPressed: () {},
-                  child: const Text("Sign in"),
+                  style: ElevatedButton.styleFrom(minimumSize: Size.infinite),
+                  child: const Text("Sign in", style: TextStyle(fontSize: 20)),
                 ),
               ),
               const SizedBox(height: 16),
               Wrap(
                 children: [
-                  const Text("Don't have an Account? "),
-                  GestureDetector(child: const Text("Sign up"))
+                  const Text("Don't have an Account? ",
+                      style: TextStyle(fontSize: 16)),
+                  GestureDetector(
+                      child: Text("Sign up",
+                          style: TextStyle(fontSize: 16, color: colorGreen)))
                 ],
               ),
               const SizedBox(height: 13),
-              Row(
-                children: [
-                  Expanded(child: Divider(thickness: 1, color: lineColor)),
-                  const SizedBox(width: 15),
-                  const Text("OR"),
-                  const SizedBox(width: 15),
-                  Expanded(child: Divider(thickness: 1, color: lineColor)),
-                ],
+              Container(
+                constraints: const BoxConstraints(maxWidth: 400),
+                child: Row(
+                  children: [
+                    Expanded(child: Divider(thickness: 1, color: lineColor)),
+                    const SizedBox(width: 15),
+                    const Text("OR"),
+                    const SizedBox(width: 15),
+                    Expanded(child: Divider(thickness: 1, color: lineColor)),
+                  ],
+                ),
               ),
               const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+              Wrap(
+                alignment: WrapAlignment.center,
                 children: [
                   circleContainer(
-                    child: Image.asset("name"),
+                    child: SvgPicture.asset(fbSvg),
                   ),
                   const SizedBox(width: 12),
                   circleContainer(
-                    child: Image.asset("name"),
+                    child: SvgPicture.asset(googleSvg),
                   ),
                   const SizedBox(width: 12),
                   circleContainer(
-                    child: Image.asset("name"),
+                    child: SvgPicture.asset(linklnSvg),
                   ),
                 ],
               )
